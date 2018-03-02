66033129b7d64c8eead4ff757704feb78e71acb7
Michael Hollister <hollister.michael@gmail.com>
2018-03-02 11:58:17 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Put this is in a cron job</h1>
+<code> /bin/bash whois_checker.sh $domain </code>
---
+++ whois_checker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DOMAIN_TO_CHECK=$1
+
+WORKING_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+source "${WORKING_DIR}/bash-logging/filehandler.sh" # logger
+# source "${WORKING_DIR}/bash-logging/streamhandler.sh" # logger
+
+filename=$(basename "$0" | sed s/\.[^\.]*$//)
+logfile="$WORKING_DIR/${filename}.log"
+
+info "starting $0"
+debug "DOMAIN_TO_CHECK = ${DOMAIN_TO_CHECK}"
+
+RESP=$(whois ${DOMAIN_TO_CHECK} | grep "Registrant Name")
+debug "RESP = \"${RESP}\""
+
+if [ -z "$RESP" ]; then
+	info "got nothing back. this domain is available. going to send a push message."
+	curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://www.michaelhollister.me/api/pushover 2>&1
+else
+	info "got something back. this domain is in use. going to do nothing."
+fi
+
+info "done"
