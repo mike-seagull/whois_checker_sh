4b8e9c0cc6101046bfc0f8d00c04a8b011aef75a
Michael Hollister <hollister.michael@gmail.com>
2018-04-25 11:10:12 -0500

    prepping for docker

--- whois_checker.sh
+++ whois_checker.sh
@@ -3,8 +3,8 @@
 DOMAIN_TO_CHECK=$1
 
 WORKING_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-source "${WORKING_DIR}/bash-logging/filehandler.sh" # logger
-# source "${WORKING_DIR}/bash-logging/streamhandler.sh" # logger
+#source "${WORKING_DIR}/bash-logging/filehandler.sh" # logger
+source "${WORKING_DIR}/bash-logging/streamhandler.sh" # logger
 
 filename=$(basename "$0" | sed s/\.[^\.]*$//)
 logfile="$WORKING_DIR/${filename}.log"
@@ -17,7 +17,8 @@ debug "RESP = \"${RESP}\""
 
 if [ -z "$RESP" ]; then
 	info "got nothing back. this domain is available. going to send a push message."
-	curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://www.michaelhollister.me/api/pushover 2>&1
+	#curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://www.michaelhollister.me/api/pushover 2>&1
+	curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://home.michaelhollister.me/api/pushover 2>&1
 else
 	info "got something back. this domain is in use. going to do nothing."
 fi
