51c783fa0bfdd495c2f2dbbbd198f93ae3571082
Michael Hollister <hollister.michael@gmail.com>
2018-06-18 10:47:26 -0500

    bad push url

--- whois_checker.sh
+++ whois_checker.sh
@@ -4,7 +4,7 @@ DOMAIN_TO_CHECK=$1
 
 WORKING_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 source "${WORKING_DIR}/bash-logging/filehandler.sh" # logger
-source "${WORKING_DIR}/bash-logging/streamhandler.sh" # logger
+#source "${WORKING_DIR}/bash-logging/streamhandler.sh" # logger
 
 #filename=$(basename "$0" | sed s/\.[^\.]*$//)
 logfile="$WORKING_DIR/logs/${DOMAIN_TO_CHECK}.log"
@@ -12,13 +12,13 @@ logfile="$WORKING_DIR/logs/${DOMAIN_TO_CHECK}.log"
 info "starting $0"
 debug "DOMAIN_TO_CHECK = ${DOMAIN_TO_CHECK}"
 
-RESP=$(whois ${DOMAIN_TO_CHECK} | grep "Registrant Name")
+RESP=$(whois ${DOMAIN_TO_CHECK} | grep "Registrar URL")
 debug "RESP = \"${RESP}\""
 
 if [ -z "$RESP" ]; then
 	info "got nothing back. this domain is available. going to send a push message."
-	#curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://www.michaelhollister.me/api/pushover 2>&1
-	curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://home.michaelhollister.me/api/pushover 2>&1
+	curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" https://michaelhollister.me/api/pushover 2>&1
+	#curl -s -o /dev/null -X POST --data "message=${DOMAIN_TO_CHECK} is available" http://home.michaelhollister.me/api/pushover 2>&1
 else
 	info "got something back. this domain is in use. going to do nothing."
 fi
